dc5d04fddc4f9bc39aad2f3c286fa0bc475ce7cc
NeoLi <hbbpbb@gmail.com>
2016-08-31 13:37:54 +0800

    encapsulation

--- 2.encapsulation/model4.js
+++ 2.encapsulation/model4.js
@@ -22,3 +22,20 @@ console.log(cat1.type); // 猫科动物
 cat1.eat(); // 吃老鼠
 
 console.log(cat1.eat == cat2.eat); //true
+
+//这个方法用来判断，某个proptotype对象和某个实例之间的关系。
+console.log(Cat.prototype.isPrototypeOf(cat1)); //true
+console.log(Cat.prototype.isPrototypeOf(cat2)); //true
+
+//每个实例对象都有一个hasOwnProperty()方法，用来判断某一个属性到底是本地属性，还是继承自prototype对象的属性。
+console.log(cat1.hasOwnProperty("name")); // true
+console.log(cat1.hasOwnProperty("type")); // false
+
+//in运算符可以用来判断，某个实例是否含有某个属性，不管是不是本地属性。
+console.log("name" in cat1); // true
+console.log("type" in cat1); // true
+
+//in运算符还可以用来遍历某个对象的所有属性。
+for (var prop in cat1) {
+    console.log("cat1[" + prop + "]=" + cat1[prop]);
+}
