4442e1654d36b06883d31b29e2248beb5e0327c3
NeoLi <hbbpbb@gmail.com>
2016-09-01 14:03:29 +0800

    es6

--- 5.es6/section5.js
+++ 5.es6/section5.js
@@ -30,3 +30,9 @@ class ColorPoint extends Point {
  ES5的继承，实质是先创造子类的实例对象this，然后再将父类的方法添加到this上面（Parent.apply(this)）。
  ES6的继承机制完全不同，实质是先创造父类的实例对象this（所以必须先调用super方法），然后再用子类的构造函数修改this。
  */
+
+let cp = new ColorPoint(25, 8, 'green');
+
+console.log(cp instanceof ColorPoint); // true
+console.log(cp instanceof Point); // true
+console.log(cp.toString());
